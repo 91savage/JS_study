ff461a11756ce31abb5853cd1882b23dee47130f
91savage <tpgns1403@hanmail.net>
2022-03-22 01:26:14 +0900

    3.21

---
+++ argument.js
@@ -0,0 +1,16 @@
+function getStringLength(str) {
+    str = str || '';
+    return str.length;
+}
+
+getStringLength();
+getStringLength('hi');
+
+function getStringLength(str = ''){
+    return str.length;
+}
+
+getStringLength();
+getStringLength('hello');
+
+console.log(getStringLength('hello'));
---
+++ false.js
@@ -0,0 +1,4 @@
+var elem = null;
+
+//console.log(elem.value); // TypeError: Cannot read property 'value' of null
+console.log(elem && elem.value); // null
